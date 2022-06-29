ebde4958a8888862f81b9ab449a4fb012fda8047
beka <bekalegesse9@gmail.com>
2022-06-29 12:58:58 +0300

    web development service done!

---
+++ JS/script.js
@@ -0,0 +1,115 @@
+const processes1 = document.querySelector('.processes1');
+const processes2 = document.querySelector('.processes2');
+const processes3 = document.querySelector('.processes3');
+const processes4 = document.querySelector('.processes4');
+const processes5 = document.querySelector('.processes5');
+const processes6 = document.querySelector('.processes6');
+
+const mainArea1 = document.querySelector('.main-area1');
+const processDescription1 = document.querySelector('.process-description1');
+const process1 = document.querySelector('.process1');
+const processNumber1 = document.querySelector('.process-number1');
+
+
+const mainArea2 = document.querySelector('.main-area2');
+const processDescription2 = document.querySelector('.process-description2');
+const process2 = document.querySelector('.process2');
+const processNumber2 = document.querySelector('.process-number2');
+
+const mainArea3 = document.querySelector('.main-area3');
+const processDescription3 = document.querySelector('.process-description3');
+const process3 = document.querySelector('.process3');
+const processNumber3 = document.querySelector('.process-number3');
+
+const mainArea4 = document.querySelector('.main-area4');
+const processDescription4 = document.querySelector('.process-description4');
+const process4 = document.querySelector('.process4');
+const processNumber4 = document.querySelector('.process-number4');
+
+const mainArea5 = document.querySelector('.main-area5');
+const processDescription5 = document.querySelector('.process-description5');
+const process5 = document.querySelector('.process5');
+const processNumber5 = document.querySelector('.process-number5');
+
+const mainArea6 = document.querySelector('.main-area6');
+const processDescription6 = document.querySelector('.process-description6');
+const process6 = document.querySelector('.process6');
+const processNumber6 = document.querySelector('.process-number6');
+
+processes1.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea1.className = 'main-area-on-hover';
+    processDescription1.className = 'process-description-on-hover';
+    process1.className = 'process-on-hover';
+    processNumber1.className = 'process-number-on-hover';
+});
+processes2.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea2.className = 'main-area-on-hover';
+    processDescription2.className = 'process-description-on-hover';
+    process2.className = 'process-on-hover';
+    processNumber2.className = 'process-number-on-hover';
+});
+processes3.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea3.className = 'main-area-on-hover';
+    processDescription3.className = 'process-description-on-hover';
+    process3.className = 'process-on-hover';
+    processNumber3.className = 'process-number-on-hover';
+});
+processes4.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea4.className = 'main-area-on-hover';
+    processDescription4.className = 'process-description-on-hover';
+    process4.className = 'process-on-hover';
+    processNumber4.className = 'process-number-on-hover';
+});
+processes5.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea5.className = 'main-area-on-hover';
+    processDescription5.className = 'process-description-on-hover';
+    process5.className = 'process-on-hover';
+    processNumber5.className = 'process-number-on-hover';
+});
+processes6.addEventListener('mouseenter', function mouseHoverEvent(){
+    mainArea6.className = 'main-area-on-hover';
+    processDescription6.className = 'process-description-on-hover';
+    process6.className = 'process-on-hover';
+    processNumber6.className = 'process-number-on-hover';
+});
+
+processes1.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea1.className = 'main-area';
+    processDescription1.className = 'process-description';
+    process1.className = 'process';
+    processNumber1.className = 'process-number';
+});
+processes2.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea2.className = 'main-area';
+    processDescription2.className = 'process-description';
+    process2.className = 'process';
+    processNumber2.className = 'process-number';
+});
+processes3.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea3.className = 'main-area';
+    processDescription3.className = 'process-description';
+    process3.className = 'process';
+    processNumber3.className = 'process-number';
+});
+processes4.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea4.className = 'main-area';
+    processDescription4.className = 'process-description';
+    process4.className = 'process';
+    processNumber4.className = 'process-number';
+});
+processes5.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea5.className = 'main-area';
+    processDescription5.className = 'process-description';
+    process5.className = 'process';
+    processNumber5.className = 'process-number';
+});
+processes6.addEventListener('mouseleave', function mouseLeaveEvent(){
+    mainArea6.className = 'main-area';
+    processDescription6.className = 'process-description';
+    process6.className = 'process';
+    processNumber6.className = 'process-number';
+});
+
+// function runMouseLeaveEvent(e){
+
+// }
